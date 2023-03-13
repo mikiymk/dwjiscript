b2b6f5766e283b750dc93e8049721bbf8baff005
mikiymk <mikiymk001@gmail.com>
2023-03-13 22:02:31 +0900

    テストを追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+mod ast {
+
+    #[test]
+    fn make_astは文字列からastを作成します() {
+        let test_case = "3 + 2";
+        let ast = make_ast(test_case);
+        let expected = Ast::ExpressionPlus {
+            left: Ast::Number { value: 3 },
+            right: Ast::Number { value: 4 },
+        };
+        assert!(ast == expected, r#"parse "3 + 2""#);
+    }
+}
